889517b9804c98185120a23d2828dd46ac9e23f4
Talvin22 <talvindzaparov1@gmail.com>
2025-03-15 17:03:03 +0100

    first commit

---
+++ src/main/java/com/dzhaparov/sevem/SevemApplication.java
@@ -0,0 +1,13 @@
+package com.dzhaparov.sevem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dzhaparov/sevem/SevemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzhaparov.sevem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
