27c6fa52a6fef4ad52989d6bc9c689eb315944bc
Talvin22 <talvindzaparov1@gmail.com>
2025-03-18 14:30:02 +0100

    user repo created

---
+++ src/main/java/com/dzhaparov/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dzhaparov.repository.user;
+
+import com.dzhaparov.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
