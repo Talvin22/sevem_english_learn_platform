fd19e6e3fa7ea39414c141914b078eabb0fab973
Talvin22 <talvindzaparov1@gmail.com>
2025-03-24 14:06:44 +0100

    UserService interface created

--- src/main/java/com/dzhaparov/service/user/UserService.java
+++ src/main/java/com/dzhaparov/service/user/UserService.java
@@ -3,6 +3,7 @@ package com.dzhaparov.service.user;
 import com.dzhaparov.dto.user.request.UserDtoRequest;
 import com.dzhaparov.dto.user.response.UserDtoByIdResponse;
 import com.dzhaparov.dto.user.response.UserDtoCreateResponse;
+import com.dzhaparov.dto.user.response.UserDtoListResponse;
 import com.dzhaparov.dto.user.response.UserDtoUpdateResponse;
 
 import java.util.List;
@@ -10,11 +11,11 @@ import java.util.List;
 public interface UserService {
     UserDtoCreateResponse createUser(UserDtoRequest userDto);
 
-    UserDtoUpdateResponse updateUser(Long id, UserDtoRequest userDto);//TODO
+    UserDtoUpdateResponse updateUser(Long id, UserDtoRequest userDto);
 
     void deleteUser(Long id);
 
     UserDtoByIdResponse getUser(Long id);
 
-    List<UserDtoResponse> getAllUsers();
+    List<UserDtoListResponse> getAllUsers();
 }
