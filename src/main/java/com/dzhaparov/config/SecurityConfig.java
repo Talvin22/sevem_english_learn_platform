a19771671f0191e66596304c38a8cbf43f2cb4fc
Talvin22 <talvindzaparov1@gmail.com>
2025-03-31 14:29:02 +0200

    SecurityFilterChain bean added

--- src/main/java/com/dzhaparov/config/SecurityConfig.java
+++ src/main/java/com/dzhaparov/config/SecurityConfig.java
@@ -2,8 +2,21 @@ package com.dzhaparov.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.swing.text.DefaultEditorKit;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //TODO
+        http.authorizeHttpRequests((authorizeRequests) ->
+               authorizeRequests.anyRequest().permitAll() );
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
