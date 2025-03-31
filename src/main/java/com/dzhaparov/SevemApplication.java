5d8c4515efc8202b6023aeff11170f2bc0be6290
Talvin22 <talvindzaparov1@gmail.com>
2025-03-31 14:23:25 +0200

    Security config  created

--- src/main/java/com/dzhaparov/SevemApplication.java
+++ src/main/java/com/dzhaparov/SevemApplication.java
@@ -1,8 +1,7 @@
-package com.dzhaparov.app;
+package com.dzhaparov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class SevemApplication {
