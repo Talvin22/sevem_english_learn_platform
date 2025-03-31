e157e3729c67c84c1bb3346bb1225367248e39f3
Talvin22 <talvindzaparov1@gmail.com>
2025-03-31 14:40:58 +0200

    dotenv added

--- src/main/java/com/dzhaparov/SevemApplication.java
+++ src/main/java/com/dzhaparov/SevemApplication.java
@@ -1,5 +1,6 @@
 package com.dzhaparov;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SevemApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+
         SpringApplication.run(SevemApplication.class, args);
+
     }
 
 }
