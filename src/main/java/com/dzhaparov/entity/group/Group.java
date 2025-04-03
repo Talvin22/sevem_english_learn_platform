2db8898ace562ceb3067a7a3b0945fd951de967c
Talvin22 <talvindzaparov1@gmail.com>
2025-04-03 13:50:35 +0200

    group entity created

---
+++ src/main/java/com/dzhaparov/entity/group/Group.java
@@ -0,0 +1,22 @@
+package com.dzhaparov.entity.group;
+
+import com.dzhaparov.entity.lesson.Lesson;
+import com.dzhaparov.entity.user.User;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "group")
+    private List<User> students;
+
+    @OneToMany(mappedBy = "group")
+    private List<Lesson> lessons;
+}
--- src/main/java/com/dzhaparov/entity/user/User.java
+++ src/main/java/com/dzhaparov/entity/user/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users") //TODO группы учеников
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
