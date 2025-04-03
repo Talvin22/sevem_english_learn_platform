fda0722037f4e8a081d852c43a436f571e0ea5d6
Talvin22 <talvindzaparov1@gmail.com>
2025-04-03 13:56:44 +0200

    entity homework improved

--- src/main/java/com/dzhaparov/entity/homework/Homework.java
+++ src/main/java/com/dzhaparov/entity/homework/Homework.java
@@ -1,9 +1,9 @@
 package com.dzhaparov.entity.homework;
 
+import com.dzhaparov.entity.group.Group;
 import com.dzhaparov.entity.lesson.Lesson;
 import com.dzhaparov.entity.user.User;
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -20,7 +20,7 @@ public class Homework {
     private Lesson lesson;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "student_id", nullable = false)
+    @JoinColumn(name = "student_id", nullable = true)
     private User student;
 
     @Enumerated(EnumType.STRING)
@@ -35,6 +35,10 @@ public class Homework {
 
     @Column(name = "submitted_at")
     private LocalDateTime submittedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = true)
+    private Group group;
 
     public Long getId() {
         return id;
@@ -92,17 +96,25 @@ public class Homework {
         this.submittedAt = submittedAt;
     }
 
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Homework homework = (Homework) o;
-        return Objects.equals(id, homework.id) && Objects.equals(lesson, homework.lesson) && Objects.equals(student, homework.student) && status == homework.status && Objects.equals(grade, homework.grade) && Objects.equals(content, homework.content) && Objects.equals(submittedAt, homework.submittedAt);
+        return Objects.equals(id, homework.id) && Objects.equals(lesson, homework.lesson) && Objects.equals(student, homework.student) && status == homework.status && Objects.equals(grade, homework.grade) && Objects.equals(content, homework.content) && Objects.equals(submittedAt, homework.submittedAt) && Objects.equals(group, homework.group);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, lesson, student, status, grade, content, submittedAt);
+        return Objects.hash(id, lesson, student, status, grade, content, submittedAt, group);
     }
 
     @Override
@@ -115,6 +127,7 @@ public class Homework {
                 ", grade=" + grade +
                 ", content='" + content + '\'' +
                 ", submittedAt=" + submittedAt +
+                ", group=" + group +
                 '}';
     }
 }
