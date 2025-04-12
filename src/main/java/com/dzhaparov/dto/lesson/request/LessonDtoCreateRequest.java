7dd72b71581c8f40ff3164cd0e365b241e90c3ce
Talvin22 <talvindzaparov1@gmail.com>
2025-04-12 19:47:04 +0200

    LessonDtoCreateRequest fixed

--- src/main/java/com/dzhaparov/dto/lesson/request/LessonDtoCreateRequest.java
+++ src/main/java/com/dzhaparov/dto/lesson/request/LessonDtoCreateRequest.java
@@ -1,19 +1,15 @@
 package com.dzhaparov.dto.lesson.request;
 
-import com.dzhaparov.entity.lesson.CancelingReasons;
 import com.dzhaparov.entity.lesson.LessonStatus;
-import com.dzhaparov.entity.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.time.ZonedDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record LessonDtoRequest(
-        Long id,
-        User teacher,
-        User student,
+public record LessonDtoCreateRequest(
+        Long teacherId,
+        Long studentId,
+        Long groupId,
         ZonedDateTime dateUtc,
-        LessonStatus status,
-        CancelingReasons cancelingReasons
-) {
-}
+        LessonStatus status
+) {}
