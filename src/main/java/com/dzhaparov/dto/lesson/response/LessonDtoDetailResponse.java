7e3dbc74a46c6013ac04df9c8974af491facf9cf
Talvin22 <talvindzaparov1@gmail.com>
2025-05-05 22:13:28 +0200

    LessonDtoDetailResponse fixed

--- src/main/java/com/dzhaparov/dto/lesson/response/LessonDtoDetailResponse.java
+++ src/main/java/com/dzhaparov/dto/lesson/response/LessonDtoDetailResponse.java
@@ -2,6 +2,7 @@ package com.dzhaparov.dto.lesson.response;
 
 import com.dzhaparov.entity.lesson.CancelingReasons;
 import com.dzhaparov.entity.lesson.LessonStatus;
+import com.dzhaparov.entity.lesson.attendance.LessonAttendanceStatus;
 
 import java.time.ZonedDateTime;
 
@@ -13,5 +14,6 @@ public record LessonDtoDetailResponse(
         ZonedDateTime dateUtc,
         LessonStatus status,
         CancelingReasons cancelingReason,
+        LessonAttendanceStatus attendanceStatus,
         com.dzhaparov.entity.lesson.attendance.CancelledBy cancelledBy
 ) {}
