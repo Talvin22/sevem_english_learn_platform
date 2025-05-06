1923e71636920e4ea64afed1af2333671bdeae72
Talvin22 <talvindzaparov1@gmail.com>
2025-05-06 14:47:50 +0200

    LessonRequestDto created

---
+++ src/main/java/com/dzhaparov/dto/lesson/request/LessonRequestDto.java
@@ -0,0 +1,9 @@
+package com.dzhaparov.dto.lesson.request;
+
+import java.time.LocalDateTime;
+
+public class LessonRequestDto {
+    private Long studentId;
+    private String groupName;
+    private LocalDateTime dateUtc;
+}
