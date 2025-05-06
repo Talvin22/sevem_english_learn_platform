3bc4725cf033a4d6b1d19afd8c17b29a730b5922
Talvin22 <talvindzaparov1@gmail.com>
2025-05-06 17:47:57 +0200

    GroupRepository: findByNameAndTeacherEmail method added

--- src/main/java/com/dzhaparov/repository/group/GroupRepository.java
+++ src/main/java/com/dzhaparov/repository/group/GroupRepository.java
@@ -2,6 +2,8 @@ package com.dzhaparov.repository.group;
 
 import com.dzhaparov.entity.group.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,6 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
     List<Group> findByTeacherId(Long teacherId);
 
     Optional<Group> findByName(String groupName);
+    @Query("SELECT g FROM Group g WHERE g.name = :name AND g.teacher.email = :email")
+    Optional<Group> findByNameAndTeacherEmail(@Param("name") String name, @Param("email") String email);
 }
