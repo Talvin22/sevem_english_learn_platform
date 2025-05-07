6173b45dc7313f7bb53b41b1638a72e2d24577c9
Talvin22 <talvindzaparov1@gmail.com>
2025-05-07 21:32:40 +0200

    LessonParticipant entity cretaed

---
+++ src/main/java/com/dzhaparov/entity/lesson/LessonParticipant.java
@@ -0,0 +1,59 @@
+package com.dzhaparov.entity.lesson;
+
+import com.dzhaparov.entity.lesson.attendance.LessonAttendanceStatus;
+import com.dzhaparov.entity.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "lesson_participant")
+public class LessonParticipant {
+
+    @EmbeddedId
+    private LessonParticipantId id = new LessonParticipantId();
+
+    @ManyToOne
+    @MapsId("lessonId")
+    @JoinColumn(name = "lesson_id")
+    private Lesson lesson;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private User student;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "attendance_status")
+    private LessonAttendanceStatus attendanceStatus;
+
+    public LessonAttendanceStatus getAttendanceStatus() {
+        return attendanceStatus;
+    }
+
+    public void setAttendanceStatus(LessonAttendanceStatus attendanceStatus) {
+        this.attendanceStatus = attendanceStatus;
+    }
+
+    public User getStudent() {
+        return student;
+    }
+
+    public void setStudent(User student) {
+        this.student = student;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    public LessonParticipantId getId() {
+        return id;
+    }
+
+    public void setId(LessonParticipantId id) {
+        this.id = id;
+    }
+}
