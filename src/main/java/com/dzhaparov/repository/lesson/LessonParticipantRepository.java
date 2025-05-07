326d4f1b684bcaa783ba863061a661a2768b6bca
Talvin22 <talvindzaparov1@gmail.com>
2025-05-07 22:17:55 +0200

    LessonParticipantRepository: findAllByLessonIn method added

--- src/main/java/com/dzhaparov/repository/lesson/LessonParticipantRepository.java
+++ src/main/java/com/dzhaparov/repository/lesson/LessonParticipantRepository.java
@@ -1,6 +1,7 @@
 package com.dzhaparov.repository.lesson;
 
 
+import com.dzhaparov.entity.lesson.Lesson;
 import com.dzhaparov.entity.lesson.LessonParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 public interface LessonParticipantRepository extends JpaRepository<LessonParticipant, Long> {
     List<LessonParticipant> findByLessonId(Long lessonId);
     List<LessonParticipant> findByStudentId(Long studentId);
+    List<LessonParticipant> findAllByLessonIn(List<Lesson> lessons);
 }
