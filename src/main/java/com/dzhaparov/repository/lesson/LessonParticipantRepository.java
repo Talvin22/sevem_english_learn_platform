280a10c0fb1aec10ffccded4819eff9d784d92ec
Talvin22 <talvindzaparov1@gmail.com>
2025-05-07 22:05:09 +0200

    LessonParticipantRepository: findByLessonId method added

--- src/main/java/com/dzhaparov/repository/lesson/LessonParticipantRepository.java
+++ src/main/java/com/dzhaparov/repository/lesson/LessonParticipantRepository.java
@@ -4,5 +4,8 @@ package com.dzhaparov.repository.lesson;
 import com.dzhaparov.entity.lesson.LessonParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LessonParticipantRepository extends JpaRepository<LessonParticipant, Long> {
+    List<LessonParticipant> findByLessonId(Long lessonId);
 }
