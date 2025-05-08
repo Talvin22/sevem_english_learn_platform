08b6ff4e47a7f3f60dadabbc1ba871f777530817
Talvin22 <talvindzaparov1@gmail.com>
2025-05-08 14:57:42 +0200

    LessonDtoDetailResponse studentNames added

--- src/main/java/com/dzhaparov/dto/lesson/response/LessonDtoDetailResponse.java
+++ src/main/java/com/dzhaparov/dto/lesson/response/LessonDtoDetailResponse.java
@@ -2,18 +2,20 @@ package com.dzhaparov.dto.lesson.response;
 
 import com.dzhaparov.entity.lesson.CancelingReasons;
 import com.dzhaparov.entity.lesson.LessonStatus;
+import com.dzhaparov.entity.lesson.attendance.CancelledBy;
 import com.dzhaparov.entity.lesson.attendance.LessonAttendanceStatus;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 public record LessonDtoDetailResponse(
         Long id,
         String teacherFullName,
-        String studentFullName,
+        List<String> studentNames,
         String groupName,
         ZonedDateTime dateUtc,
         LessonStatus status,
         CancelingReasons cancelingReason,
         LessonAttendanceStatus attendanceStatus,
-        com.dzhaparov.entity.lesson.attendance.CancelledBy cancelledBy
+        CancelledBy cancelledBy
 ) {}
