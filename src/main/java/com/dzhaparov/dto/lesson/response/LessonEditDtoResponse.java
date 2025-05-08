64f67b1082cddc353575f066b31ebbc910a94ba0
Talvin22 <talvindzaparov1@gmail.com>
2025-05-08 15:23:43 +0200

    LessonEditDtoResponse chanaged

--- src/main/java/com/dzhaparov/dto/lesson/response/LessonEditDtoResponse.java
+++ src/main/java/com/dzhaparov/dto/lesson/response/LessonEditDtoResponse.java
@@ -1,9 +1,9 @@
 package com.dzhaparov.dto.lesson.response;
 
+import com.dzhaparov.dto.student.StudentAttendanceDto;
 import com.dzhaparov.entity.lesson.CancelingReasons;
 import com.dzhaparov.entity.lesson.LessonStatus;
 import com.dzhaparov.entity.lesson.attendance.CancelledBy;
-import com.dzhaparov.entity.lesson.attendance.LessonAttendanceStatus;
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -17,5 +17,5 @@ public record LessonEditDtoResponse(
         LessonStatus status,
         CancelingReasons cancelingReason,
         CancelledBy cancelledBy,
-        LessonAttendanceStatus attendanceStatus
+        List<StudentAttendanceDto> studentAttendanceList
 ) {}
