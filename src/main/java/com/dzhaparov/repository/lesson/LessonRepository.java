327a901b2cef486b536c9555fbdbe496113f6d61
Talvin22 <talvindzaparov1@gmail.com>
2025-05-09 18:39:17 +0200

    Lesson repo: findByTeacherIdAndDateUtcBetweenOrderByDateUtcAsc method created

--- src/main/java/com/dzhaparov/repository/lesson/LessonRepository.java
+++ src/main/java/com/dzhaparov/repository/lesson/LessonRepository.java
@@ -4,9 +4,11 @@ import com.dzhaparov.entity.lesson.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
     List<Lesson> findByTeacherId(Long teacherId);
+    List<Lesson> findByTeacherIdAndDateUtcBetweenOrderByDateUtcAsc(Long teacherId, ZonedDateTime start, ZonedDateTime end);
 }
