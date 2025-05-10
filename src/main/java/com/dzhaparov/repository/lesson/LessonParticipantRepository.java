501474cd5594f230890d4d2391eada19ae3a3d86
Talvin22 <talvindzaparov1@gmail.com>
2025-05-10 17:47:20 +0200

    LessonParticipantRepo: deleteAllByLessonId method created

--- src/main/java/com/dzhaparov/repository/lesson/LessonParticipantRepository.java
+++ src/main/java/com/dzhaparov/repository/lesson/LessonParticipantRepository.java
@@ -4,6 +4,10 @@ package com.dzhaparov.repository.lesson;
 import com.dzhaparov.entity.lesson.Lesson;
 import com.dzhaparov.entity.lesson.LessonParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,4 +18,8 @@ public interface LessonParticipantRepository extends JpaRepository<LessonPartici
     List<LessonParticipant> findAllByStudentId(Long studentId);
     List<LessonParticipant> findByLesson(Lesson lesson);
     List<LessonParticipant> findAllByLesson(Lesson lesson);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM LessonParticipant lp WHERE lp.id.lessonId = :lessonId")
+    void deleteAllByLessonId(@Param("lessonId") Long lessonId);
 }
