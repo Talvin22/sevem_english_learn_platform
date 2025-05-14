525de1613bb95bea0c0a0f5e5cb5e4449593baf7
Talvin22 <talvindzaparov1@gmail.com>
2025-05-14 19:43:39 +0200

    HomeworkController created

---
+++ src/main/java/com/dzhaparov/controller/homework/HomeworkController.java
@@ -0,0 +1,25 @@
+package com.dzhaparov.controller.homework;
+
+import com.dzhaparov.util.AuthHelper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/homeworks")
+public class HomeworkController {
+
+    private final HomeworkService homeworkService;
+    private final AuthHelper authHelper;
+
+    public HomeworkController(HomeworkService homeworkService, AuthHelper authHelper) {
+        this.homeworkService = homeworkService;
+        this.authHelper = authHelper;
+    }
+
+    @PostMapping
+    public ResponseEntity<HomeworkDtoResponse> createHomework(@RequestBody CreateHomeworkRequest request) {
+        Long teacherId = authHelper.getCurrentUser().getId();
+        HomeworkDtoResponse response = homeworkService.createHomework(request, teacherId);
+        return ResponseEntity.ok(response);
+    }
+}
