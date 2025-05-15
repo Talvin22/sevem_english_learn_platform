dbd2f4dfdf91833d94b1f4e3bc0f9e3aab961326
Talvin22 <talvindzaparov1@gmail.com>
2025-05-15 10:55:00 +0200

    HomeworkDtoDetailResponse updated

--- src/main/java/com/dzhaparov/dto/homework/response/HomeworkDtoDetailResponse.java
+++ src/main/java/com/dzhaparov/dto/homework/response/HomeworkDtoDetailResponse.java
@@ -1,6 +1,5 @@
 package com.dzhaparov.dto.homework.response;
 
-import com.dzhaparov.entity.homework.Homework;
 import com.dzhaparov.entity.homework.HomeworkStatus;
 
 import java.time.LocalDateTime;
@@ -8,21 +7,9 @@ import java.time.LocalDateTime;
 public record HomeworkDtoDetailResponse(
         Long id,
         Long lessonId,
+        String groupName,
         LocalDateTime lessonDate,
         HomeworkStatus status,
         Integer grade,
-        String content,
-        String groupName
-) {
-    public static HomeworkDtoDetailResponse from(Homework hw) {
-        return new HomeworkDtoDetailResponse(
-                hw.getId(),
-                hw.getLesson().getId(),
-                hw.getLesson().getDateUtc().toLocalDateTime(),
-                hw.getStatus(),
-                hw.getGrade(),
-                hw.getContent(),
-                hw.getGroup() != null ? hw.getGroup().getName() : null
-        );
-    }
-}
+        String content
+) {}
